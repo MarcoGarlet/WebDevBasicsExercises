4ff00f758867215d6fda4fd9d8e46605bd0ecc4a
MatteoNocentini04 <nocentinimatteo04@gmail.com>
2024-05-03 18:42:48 +0200

    Aggiunto esercizio dado

--- lez4/ReactEs1/src/components/Dado.js
+++ lez4/ReactEs1/src/components/Dado.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 import welcomeImg from '../img/welcomeD.jpg';
+import primaF from '../img/primaFaccia.jpg';
+import secondaF from '../img/secondaFaccia.jpg';
+import terzaF from '../img/terzaFaccia.jpg';
+import quartaF from '../img/quartaFaccia.jpg';
+import quintaF from '../img/quintaFaccia.jpg';
+import sestaF from '../img/sestaFaccia.jpg';
+
+const sides = [primaF,secondaF,terzaF,quartaF,quintaF,sestaF];  
 export default class Dado extends React.Component{
     constructor(props){
         super(props);
-        this.state = {numero:"0"};
+        this.state = {numero:0};
     }
     estrazione(){
         return Math.round(Math.random()*5)+1;
@@ -13,12 +21,12 @@ export default class Dado extends React.Component{
         this.setState({numero: this.estrazione()});
     }
     render(){
-        let valore;
-        if (parseInt(this.state.numero) == 0){
-            valore = <img src={welcomeImg} style={{maxWidth:'20%'}}/>;
-        } else{
-            valore = <div>{this.state.numero}</div>
+        let welcome = <img src={welcomeImg} style={{maxWidth:'20%'}}/>;
+        let esito = this.state.numero;
+        if(esito != 0){
+            esito =  <img src={sides[esito-1]} style={{maxWidth:'20%'}}/>;
         }
-        return <div><p>{valore}</p><button onClick={ ()=>this.lancio()  }>Lancia il Dado</button></div>
+        let valore = (this.state.numero == 0)?<div/>: <div>{esito}</div>
+        return <div><p>{welcome}</p><p>{valore}</p><button onClick={ ()=>this.lancio()  }>Lancia il Dado</button></div>
     }
 }
